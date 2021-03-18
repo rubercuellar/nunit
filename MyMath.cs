d53a4525668fdf3cb2a76a767938b423b454c708
Ruber <ruber.cuellar@neasure.net>
2021-03-18 14:31:43 -0400

    testing

---
+++ MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitDem
+{
+    public class MyMath
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ MyTestCase.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NunitDem
+{
+    [TestFixture]
+    class MyTestCase
+    {
+        [TestCase]
+        public void Add()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(31, math.Add(20, 11));
+        }
+
+        [TestCase]
+        public void Sub()
+        {
+            MyMath math = new MyMath();
+            Assert.AreEqual(10, math.Sub(20, 10));
+        }
+    }
+}
